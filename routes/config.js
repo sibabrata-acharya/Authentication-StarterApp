28331ec75058b32bfe081e566f925fb82ff5ae61
TMall <Suryakala.Botta@cognizant.com>
2016-05-17 17:50:06 +0530

    We have uploaded your required codebase

--- routes/config.js
+++ routes/config.js
@@ -1,4 +1,4 @@
-var URIs = { "redirecturi_homepage" : "http://suryakalaApp22.54.208.194.189.xip.io/userpage",
-"redirecturi" : "http://suryakalaApp22.54.208.194.189.xip.io"
+var URIs = { "redirecturi_homepage" : "http://suryakalaApp23.54.208.194.189.xip.io/userpage",
+"redirecturi" : "http://suryakalaApp23.54.208.194.189.xip.io"
 };
 module.exports = URIs;
