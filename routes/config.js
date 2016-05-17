3e7e70f289216d363d0c99162b4d834e8d9afd76
TMall <Suryakala.Botta@cognizant.com>
2016-05-17 16:53:28 +0530

    We have uploaded your required codebase

--- routes/config.js
+++ routes/config.js
@@ -1,4 +1,4 @@
-var URIs = { "redirecturi_homepage" : "http://OrderApp.54.208.194.189.xip.io/userpage",
-"redirecturi" : "http://OrderApp.54.208.194.189.xip.io"
+var URIs = { "redirecturi_homepage" : "http://BillingApp.54.208.194.189.xip.io/userpage",
+"redirecturi" : "http://BillingApp.54.208.194.189.xip.io"
 };
 module.exports = URIs;
